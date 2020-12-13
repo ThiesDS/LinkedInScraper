bdcf3be9d94dbc8a8c15a279a309637aeceda7b2
Sven Thies <sven.thies@infomotion.de>
2020-12-13 15:33:27 +0100

    Replace .env with explicitly set env variables.

--- src/app/main.py
+++ src/app/main.py
@@ -1,35 +1,27 @@
+import os
 import json
 import sys
 import time
 import xlsxwriter
-from configparser import ConfigParser
 
 from HashtagScraper import HashtagScraper
 
-# Loading of configurations
-from utils import ComplexEncoder
-
-config = ConfigParser()
-config.read('config.ini')
-
-# Setting the execution mode
-headless_option = 'HEADLESS'
-
 # Loading of input data (LinkedIn hashtag Urls)
 hashtag_urls = []
-for entry in open('../input/hashtags/urls.txt'), "r"):
+for entry in open('../input/hashtags/urls.txt', "r"):
     hashtag_urls.append(entry.strip())
 
-#if len(hashtag_urls) == 0:
-#    print("Please provide an input.")
-#    sys.exit(0)
+# Warning if hashtags are not provided
+if len(hashtag_urls) == 0:
+    print("Please provide an input.")
+    sys.exit(0)
 
-# Launch Scraper
+# Launch HashtagScraper
 s = HashtagScraper(
-    linkedin_username=config.get('linkedin', 'username'),
-    linkedin_password=config.get('linkedin', 'password'),
+    linkedin_username=os.getenv('LINKEDIN_EMAIL'),
+    linkedin_password=os.getenv('LINKEDIN_PASSWORD'),
     hashtag_urls=hashtag_urls,
-    headless=headless_option
+    headless='HEADLESS'
 )
 
 s.start()
