b64cc2c64d0c4f47e3b1e9ba74c0e2faa8b06ca2
Sven Thies <sven.thies@infomotion.de>
2021-01-15 13:48:30 +0100

    Fix bug.

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag linkedinscraper sventhies/linkedinscraper:$(git describe --abbrev=0 --tags)
-docker push sventhies/linkedinscraper
+docker push sventhies/linkedinscraper:$(git describe --abbrev=0 --tags)
