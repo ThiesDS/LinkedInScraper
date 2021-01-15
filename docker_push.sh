8d278664f0d8998cbb06df5a2f70ed438e075a0f
Sven Thies <sven.thies@infomotion.de>
2021-01-15 11:47:44 +0100

    Create pipeline.

--- docker_push.sh
+++ docker_push.sh
@@ -2,6 +2,4 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker tag linkedinscraper sventhies/linkedinscraper:latest
-
-docker push sventhies/linkedinscraper
+docker push sventhies/linkedinscraper:$(git describe --abbrev=0 --tags)
