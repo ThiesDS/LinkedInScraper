994eb6e92f965edc05f5d2ce8ea5352a7b638197
Sven Thies <sven.thies@infomotion.de>
2021-01-15 14:01:40 +0100

    Add debug info.

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag linkedinscraper sventhies/linkedinscraper:$(git describe --abbrev=0 --tags)
+docker images
 docker push sventhies/linkedinscraper:$(git describe --abbrev=0 --tags)
